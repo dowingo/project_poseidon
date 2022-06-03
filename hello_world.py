853ea0f1b982051baf29dd5b24a150b6d926fd11
domi <100360112+dowingo@users.noreply.github.com>
2022-06-03 11:44:23 +0200

    Formular ist fertig JSON Datei wurde erstellt. Sämtliche Informationen welche im Formular erfasst werden, werden nun automatisch in einer JSON Datei gespeichert.

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,12 @@
-print("Hello World")
+def hallo(name="Welt", lang="en"):
+    if lang == "en":
+        print("Hello" + na,e + "!")
+    elif lang == "de":
+        print("Hallo " + name + "!")
+    elif lang == "ch":
+        print("Salü " + name + "!")
+    else:
+        print("Tagwohl " + name + "!")
+
+hallo(name="Domingo", lang="x")
 
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import random
+import json
 
 from flask import Flask
 from flask import render_template
 from flask import url_for
+from flask import request
+import random
+
+
 
 app = Flask(__name__)
 
@@ -13,11 +17,33 @@ def hello():
     about_link = url_for("about")
     return render_template("index.html", name=name_choice_, link=about_link) #das wird dem Browser gesendet, wenn die URL aufgerufen wird
 
-
 @app.route("/about")
 def about():
     return "<h1>about test</h1>"
 
+@app.route('/formular', methods=["GET", "POST"])
+def form():
+    if request.method == "POST":
+        data = request.form
+        einkauf = data["einkauf"]
+        datum = data["datum"]
+        preis = data["preis"]
+        kat = data["kat"]
+
+        try:
+            with open("data.json", "r") as open_file:
+                inhalt = json.load(open_file)
+        except FileNotFoundError:
+            inhalt = []
+
+        dict = {"Einkauf": einkauf, "Datum": datum, "Kategorie": kat, "Preis CHF": preis}
+        inhalt.append(dict)
+
+        with open("data.json", "w") as open_file:
+            json.dump(inhalt, open_file, indent=4)
+        return render_template("index.html")
+    else:
+        render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
